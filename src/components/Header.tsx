2cbaee48bf83259394ccce20de489620984b66d2
Diego Fernandes <diego.schell.f@gmail.com>
2021-02-04 22:24:34 -0300

    chore: Search box

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,10 @@
-import { Flex, Avatar, AvatarBadge, Box, Input, Text } from "@chakra-ui/react";
+import { useState } from "react";
+import { Flex, Avatar, AvatarBadge, Box, Input, Text, SimpleGrid, Link } from "@chakra-ui/react";
 import { RiSearchLine } from "react-icons/ri";
 
 export function Header() {
+  const [searchInput, setSearchInput] = useState('');
+
   return (
     <Flex 
       as="header" 
@@ -21,15 +24,93 @@ export function Header() {
         </Box>
       </Flex>
 
-      <Flex as="label" cursor="text" flex="1" ml="8" alignSelf="center" color="purple.500">
+      <Flex as="label" cursor="text" flex="1" ml="8" alignSelf="center" color="purple.500" position="relative">
         <RiSearchLine size="20" />
         <Input 
           variant="unstyled"
           color="gray.700"
           placeholder="Buscar por contato, tag, form, etc..." 
           paddingX="4"
+          onChange={e => setSearchInput(e.target.value)}
+          value={searchInput}
         />
+
+        { !!searchInput && (
+          <Box
+            bgColor="white" 
+            p="8"
+            shadow="0 0 20px rgba(0, 0, 0, 0.1)"
+            zIndex={10}
+            position="absolute"
+            left="0"
+            top="100%"
+            mt="4"
+            borderRadius={4}
+            w="2xl"
+          >
+            <Text fontWeight="bold" color="gray.700" fontSize="sm">Resultados para "die"</Text>
+
+            <SimpleGrid mt="4" columns={3} spacing="4">
+              <Link>
+                <Text color="gray.500" fontWeight="bold" fontSize="xs">INSCRITO</Text>
+                <Text fontWeight="medium" fontSize="sm" color="gray.800">
+                  <Text as="mark" bgColor="purple.100">Die</Text>go Fernandes
+                </Text>
+              </Link>
+              <Link>
+                <Text color="gray.500" fontWeight="bold" fontSize="xs">INSCRITO</Text>
+                <Text fontWeight="medium" fontSize="sm" color="gray.800">
+                  <Text as="mark" bgColor="purple.100">Die</Text>go Fernandes
+                </Text>
+              </Link>
+              <Link>
+                <Text color="gray.500" fontWeight="bold" fontSize="xs">INSCRITO</Text>
+                <Text fontWeight="medium" fontSize="sm" color="gray.800">
+                  <Text as="mark" bgColor="purple.100">Die</Text>go Fernandes
+                </Text>
+              </Link>
+              <Link>
+                <Text color="gray.500" fontWeight="bold" fontSize="xs">INSCRITO</Text>
+                <Text fontWeight="medium" fontSize="sm" color="gray.800">
+                  <Text as="mark" bgColor="purple.100">Die</Text>go Fernandes
+                </Text>
+              </Link>
+              <Link>
+                <Text color="gray.500" fontWeight="bold" fontSize="xs">INSCRITO</Text>
+                <Text fontWeight="medium" fontSize="sm" color="gray.800">
+                  <Text as="mark" bgColor="purple.100">Die</Text>go Fernandes
+                </Text>
+              </Link>
+              <Link>
+                <Text color="gray.500" fontWeight="bold" fontSize="xs">INSCRITO</Text>
+                <Text fontWeight="medium" fontSize="sm" color="gray.800">
+                  <Text as="mark" bgColor="purple.100">Die</Text>go Fernandes
+                </Text>
+              </Link>
+              <Link>
+                <Text color="gray.500" fontWeight="bold" fontSize="xs">INSCRITO</Text>
+                <Text fontWeight="medium" fontSize="sm" color="gray.800">
+                  <Text as="mark" bgColor="purple.100">Die</Text>go Fernandes
+                </Text>
+              </Link>
+              <Link>
+                <Text color="gray.500" fontWeight="bold" fontSize="xs">INSCRITO</Text>
+                <Text fontWeight="medium" fontSize="sm" color="gray.800">
+                  <Text as="mark" bgColor="purple.100">Die</Text>go Fernandes
+                </Text>
+              </Link>
+              <Link>
+                <Text color="gray.500" fontWeight="bold" fontSize="xs">INSCRITO</Text>
+                <Text fontWeight="medium" fontSize="sm" color="gray.800">
+                  <Text as="mark" bgColor="purple.100">Die</Text>go Fernandes
+                </Text>
+              </Link>
+            </SimpleGrid>
+          </Box>
+        ) }
       </Flex>
+    
+      
     </Flex>
   );
 }
