ba4b649f7bc9c52ae0e6121cd1fb4b29402e4e22
Joseph Oliveira <josephvalenteoliveira@gmail.com>
2021-07-14 16:43:21 -0300

    feat: Add useTemplates hook

---
+++ src/services/hooks/useTemplates.ts
@@ -0,0 +1,42 @@
+import { useQuery, UseQueryOptions, UseQueryResult } from 'react-query';
+import { api } from '../api';
+
+type Template = {
+  id: string;
+  title: string;
+  isDefault: boolean;
+};
+
+type GetTemplatesReponse = {
+  templates: Template[];
+};
+
+export async function getTemplates(page: number, searchQuery?: string): Promise<GetTemplatesReponse>  {
+  const response = await api.get('/templates/search', {
+    params: {
+      page,
+      query: searchQuery
+    }
+  });
+
+  const { data } = response;
+
+  const templates = data.map(template => {
+    return {
+      id: template.id,
+      title: template.title,
+      isDefault: template.isDefault
+    };
+  });
+
+  return {
+    templates
+  };
+}
+
+export function useTemplates(page: number = 1, searchQuery?: string, options?: UseQueryOptions) {
+  return useQuery(['templates', [page, searchQuery]], () => getTemplates(page, searchQuery), {
+    staleTime: 1000 * 60 * 1,
+    ...options
+  }) as UseQueryResult<GetTemplatesReponse, unknown> ;
+}
