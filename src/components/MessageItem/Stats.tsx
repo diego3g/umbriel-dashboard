a1275fac3b46dea5022a00d8ce3173217913dc46
Joseph Oliveira <josephvalenteoliveira@gmail.com>
2021-08-09 16:50:43 -0300

    chore: Add text to specify the numbers on MessageStats

--- src/components/MessageItem/Stats.tsx
+++ src/components/MessageItem/Stats.tsx
@@ -10,10 +10,10 @@ export function MessageStats({ messageId }: MessageStatsProps) {
 
   return (
     <HStack mt="3" spacing="3" divider={<StackDivider borderColor="gray.200" />}>
-      {data?.stats.recipientsCount && <Text color="gray.500">{data?.stats.recipientsCount}</Text>}
-      {data?.stats.openRate && <Text color="gray.500">{data?.stats.openRate}</Text>}
-      {data?.stats.clickRate && <Text color="gray.500">{data?.stats.clickRate}</Text>}
-      {data?.stats.clickCount && <Text color="gray.500">{data?.stats.clickCount}</Text>}
+      {data?.stats.recipientsCount && <Text color="gray.500">{`${data?.stats.recipientsCount} destinatários`}</Text>}
+      {data?.stats.openRate && <Text color="gray.500">{`${data?.stats.openRate}% de abertura`}</Text>}
+      {data?.stats.clickRate && <Text color="gray.500">{`${data?.stats.clickRate}% de clicks`}</Text>}
+      {data?.stats.clickCount && <Text color="gray.500">{`${data?.stats.clickCount} clicks`}</Text>}
     </HStack>
   )
 }
